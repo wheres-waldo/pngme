89cb87736517c0d88656d3e4371655229c4f8f87
Steven Waldron <steven.waldron@pm.me>
2020-09-19 16:07:34 -0500

    Created all of the supported command functions

--- src/commands.rs
+++ src/commands.rs
@@ -14,3 +14,65 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with pngme.  If not, see <http://www.gnu.org/licenses/>.
+use crate::{
+    args::{Decode, Encode, Print, Remove},
+    chunk::Chunk,
+    png::Png,
+};
+use anyhow::{Error, Result};
+use std::{convert::TryFrom, fs};
+
+/// Encodes a `Chunk` into a `Png` file
+pub fn encode(opts: Encode) -> Result<()> {
+    let buffer = fs::read(&opts.file)?;
+
+    let mut png = Png::try_from(&buffer[..])?;
+    let chunk = Chunk::new(opts.chunk_type.as_str(), opts.message.as_str())?;
+    png.append_chunk(chunk);
+
+    let output_file = &opts.output.unwrap_or(opts.file);
+    fs::write(output_file, &png.as_bytes()[..])?;
+
+    Ok(())
+}
+
+/// Decode a `Chunk` from a `Png` file
+pub fn decode(opts: Decode) -> Result<String> {
+    let buffer = fs::read(&opts.file)?;
+
+    let png = Png::try_from(&buffer[..])?;
+    let chunk = png
+        .chunk_by_type(&opts.chunk_type.as_str())
+        .ok_or_else(|| {
+            Error::msg(format!(
+                "Chunk type {} was not found in file",
+                &opts.chunk_type
+            ))
+        })?;
+
+    Ok(String::from_utf8(chunk.data().to_vec())?)
+}
+
+/// Removes the first `Chunk` with `chunk_type` from a `Png` file
+pub fn remove(opts: Remove) -> Result<()> {
+    let buffer = fs::read(&opts.file)?;
+
+    let mut png = Png::try_from(&buffer[..])?;
+    png.remove_chunk(&opts.chunk_type.as_str())?;
+
+    fs::write(&opts.file, png.as_bytes())?;
+    Ok(())
+}
+
+/// Returns a `String` that represents all of the `Chunks` in a `Png` file
+pub fn print(opts: Print) -> Result<String> {
+    let buffer = fs::read(&opts.file)?;
+
+    let png = Png::try_from(&buffer[..])?;
+    Ok(png
+        .chunks()
+        .iter()
+        .map(|chunk| chunk.to_string())
+        .collect::<Vec<String>>()
+        .join("\n\n"))
+}
